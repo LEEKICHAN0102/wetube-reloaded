407e7a4d6b1a9a365b7a6ad90d41a5d6d8169f9b
Kichan <rlcks01537@gmail.com>
2023-03-10 13:13:30 +0900

    2023-03-10 wetube-clone-coding commit

--- src/server.js
+++ src/server.js
@@ -1,33 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+app.use(logger);
 
-const privateMiddleWare = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed Continue");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("FinalWare");
-};
-
-app.get("/", logger, handleHome);
-
-// Use Valuable or Inline Function
-// app.get("/",handleHome(req,res){
-// res.send("Hello World")})
-// It is your choice!
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server Listening on port http://localhost:${PORT}👀`);
