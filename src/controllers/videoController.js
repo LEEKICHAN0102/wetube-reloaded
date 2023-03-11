d205689555e786aae23fca38dfe0659dd005a244
Kichan <rlcks01537@gmail.com>
2023-03-11 13:53:45 +0900

    2023-03-11 wetube-clone-coding commit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,6 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => {
-  return res.send("Edit Videos");
-};
+export const trending = (req, res) => res.render("home", { pagetitle: "Home" });
+export const see = (req, res) => res.render("watch", { pagetitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pagetitle: "Edit" });
 export const search = (req, res) => res.send("Video Search");
 export const upload = (req, res) => res.send("Video Upload");
 export const deleteVideo = (req, res) => {
