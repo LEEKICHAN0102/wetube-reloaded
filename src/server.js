caf6d2894df922a0c6fd7df4d0d9fd93808bfaa7
Kichan <rlcks01537@gmail.com>
2023-03-17 15:06:49 +0900

    2023-03-17 wetube-clone-coding commit

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -18,6 +19,6 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 const handleListening = () =>
-  console.log(`Server Listening on port http://localhost:${PORT}👀`);
+  console.log(`✔ Server Listening on port http://localhost:${PORT}👀`);
 
 app.listen(PORT, handleListening);
