b99b3f2c1dfbea599a517f8d59b1d4a35cff4207
Kichan <rlcks01537@gmail.com>
2023-03-18 12:06:43 +0900

    2023-03-18 wetube-clone-coding commit

--- src/init.js
+++ src/init.js
@@ -5,6 +5,6 @@ import app from "./server";
 const PORT = 4000;
 
 const handleListening = () =>
-  console.log(`✔ Server Listening on port http://localhost:${PORT}👀`);
+  console.log(`✅ Server Listening on http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
