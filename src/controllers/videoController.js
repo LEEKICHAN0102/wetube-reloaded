c95012a73da78340783fdb4906d847b4020a7c9e
Kichan <rlcks01537@gmail.com>
2023-03-20 11:24:22 +0900

    2023-03-20 wetube-clone-coding commit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,13 +2,13 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
-  console.log(videos);
   res.render("home", { pageTitle: "Home", videos });
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params; //==const id = req.params.id;
-  return res.render("watch", { pageTitle: `Watching` });
+  const video = await Video.findById(id);
+  return res.render("watch", { pageTitle: video.title, video });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
