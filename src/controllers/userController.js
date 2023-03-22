9b07567b869a5313c91179537fcf9ace47ffba07
Kichan <rlcks01537@gmail.com>
2023-03-22 13:52:06 +0900

    2023-03-22 wetube-clone-coding commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,29 +5,48 @@ export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
   const pageTitle = "Join";
   if (password !== password2) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "Password confirmation does not match.",
     });
   }
   const exists = await User.exists({ $or: [{ username }, { email }] });
   if (exists) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "This username/email is already used.",
     });
   }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
+};
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const exists = await User.exists({ username });
+  if (!exists) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "An account with username is does not exist..",
+    });
+  }
+  res.end();
 };
 export const edit = (req, res) => res.send("Edit User!");
 export const remove = (req, res) => res.send("Remove User!");
-export const login = (req, res) => res.send("User Login!");
 export const logout = (req, res) => res.send("User Logout");
 export const see = (req, res) => res.send("See User");
