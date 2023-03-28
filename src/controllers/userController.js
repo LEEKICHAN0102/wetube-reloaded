57faf817e4ce0b3f7de3f9f990e3d1902e138dba
Kichan <rlcks01537@gmail.com>
2023-03-28 15:43:46 +0900

    2023-03-28 wetube-clone-coding

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import bcryptjs from "bcryptjs";
+import { application } from "express";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -60,7 +61,8 @@ export const postLogin = async (req, res) => {
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    client_id: "f0040a90163c1eddc185",
+    client_id: process.env.GH_CLIENT,
+    GH_CLIENT,
     allow_signup: false,
     scope: "read:user user:email",
   };
@@ -69,7 +71,24 @@ export const startGithubLogin = (req, res) => {
   return res.redirect(finalUrl);
 };
 
-export const finishGithubLogin = (req, res) => {};
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const data = await fetch(finalUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const json = await data.json();
+  console.log(json);
+};
 export const edit = (req, res) => res.send("Edit User!");
 export const remove = (req, res) => res.send("Remove User!");
 export const logout = (req, res) => res.send("User Logout");
