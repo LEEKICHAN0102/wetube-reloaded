484339655b47cbe17fb6b40ebca773b9fafa4cd2
Kichan <rlcks01537@gmail.com>
2023-03-29 15:59:50 +0900

    2023-03-29 wetube-clone-coding commit

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {
   edit,
-  remove,
   logout,
   see,
   startGithubLogin,
@@ -10,11 +9,10 @@ import {
 
 const userRouter = express.Router();
 
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
 userRouter.get(":id", see);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
-userRouter.get("/logout", logout);
 
 export default userRouter;
