3e94bb3ab60c5feb193c86aaea69542a419f443f
Kichan <rlcks01537@gmail.com>
2023-04-02 13:21:29 +0900

    2023-04-02 wetube-clone-coding commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import Video from "../models/Video";
 import bcryptjs from "bcryptjs";
 import bcrypt from "bcryptjs/dist/bcrypt";
 import { application } from "express";
@@ -208,8 +209,10 @@ export const see = async (req, res) => {
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found..." });
   }
+  const videos = await Video.find({ owner: user._id });
   return res.render("users/profile", {
     pageTitle: user.name,
     user,
+    videos,
   });
 };
