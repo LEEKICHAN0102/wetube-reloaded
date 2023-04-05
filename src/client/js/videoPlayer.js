88b3d6e083a840d457a4001a6b563a19ee6149cf
Kichan <rlcks01537@gmail.com>
2023-04-05 15:40:24 +0900

    2023-04-05 wetube-clone-coding commit

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,7 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handelPlayClick = () => {
   if (video.paused) {
@@ -10,18 +10,18 @@ const handelPlayClick = () => {
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handlePlay = () => {
-  playBtn.innerText = "Pause";
+const handelMuteClick = () => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmuted" : "Muted";
+  volumeRange.value = video.muted ? "0" : "0.5";
 };
-const handlePause = () => {
-  playBtn.innerText = "Play";
-};
-
-const handelMute = () => {};
 
 playBtn.addEventListener("click", handelPlayClick);
-muteBtn.addEventListener("click", handelMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+muteBtn.addEventListener("click", handelMuteClick);
