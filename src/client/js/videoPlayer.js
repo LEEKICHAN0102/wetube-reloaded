3d7c92fe3f6c07a7ef63b373bd60a52473f75e71
Kichan <rlcks01537@gmail.com>
2023-04-05 15:24:02 +0900

    2023-04-05 wetube-clone-coding commit

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,27 @@
-console.log("video Player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handelPlayClick = () => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const handlePlay = () => {
+  playBtn.innerText = "Pause";
+};
+const handlePause = () => {
+  playBtn.innerText = "Play";
+};
+
+const handelMute = () => {};
+
+playBtn.addEventListener("click", handelPlayClick);
+muteBtn.addEventListener("click", handelMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
