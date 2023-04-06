4e1461014a6a1691b408cf0a13b8f7e163b5c0a7
Kichan <rlcks01537@gmail.com>
2023-04-06 19:28:08 +0900

    2023-04-06 wetube-clone-coding commit

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,12 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
 
 const handelPlayClick = () => {
   if (video.paused) {
@@ -20,8 +24,31 @@ const handelMuteClick = () => {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? "Unmuted" : "Muted";
-  volumeRange.value = video.muted ? "0" : "0.5";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+};
+
+const handleLoadedMetadata = () => {
+  totalTime.innerText = Math.floor(video.duration);
+};
+
+const handleLoadedTimeUpdate = () => {
+  currentTime.innerText = Math.floor(video.currentTime);
 };
 
 playBtn.addEventListener("click", handelPlayClick);
 muteBtn.addEventListener("click", handelMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleLoadedTimeUpdate);
